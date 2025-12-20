ecf437133f8e856511f2913e44684542833fb8f7
steve button <info@contentthatmoves.com>
2025-12-20 10:12:44 +0000

    File changes

--- src/components/storymap/ChapterNavigation.jsx
+++ src/components/storymap/ChapterNavigation.jsx
@@ -6,7 +6,7 @@ import { MapPin } from 'lucide-react';
 export default function ChapterNavigation({ chapters, activeIndex, onNavigate, isOpen }) {
     return (
         <motion.div 
-            className="fixed left-6 top-[200px] z-50 hidden md:block"
+            className="fixed left-6 top-[200px] z-[120] hidden md:block"
             initial={{ x: -300, opacity: 0 }}
             animate={{ x: isOpen ? 0 : -300, opacity: isOpen ? 1 : 0 }}
             exit={{ x: -300, opacity: 0 }}
